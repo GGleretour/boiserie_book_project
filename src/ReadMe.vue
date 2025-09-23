502134b5c5dad374b3a539cd7f068d5703ac5479
guillaume mondou <guillaume.mondou@epitech.eu>
2025-09-23 18:03:06 +0200

    hot-fix: small patch

--- src/ReadMe.vue
+++ src/ReadMe.vue
@@ -5,7 +5,7 @@
       </div>
     <img
         id="readme_page"
-        src="./assets/pages/page.png"
+        src="./assets/page.png"
         alt="read me page"
         width="600"
         height="auto"
