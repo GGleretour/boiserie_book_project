d0a9c6eca01990131e3a0fa2121ac267fa73a71c
guillaume mondou <guillaume.mondou@epitech.eu>
2025-09-30 12:30:29 +0200

    feat: add more cube for itch type

--- src/cubes.js
+++ src/cubes.js
@@ -46,5 +46,53 @@ export const cubesinfos = [
     page: 0, // Apparaît sur la première double-page (pages 1-2)
     side: 'left', // Apparaît sur la page de gauche
     type:'kitchenReceptacle'
+  },
+  {
+    id:"cube-4",
+    img_src:'assets/block_charbon.png',
+    find: false,
+    stocked:false,
+    opacity: 0.2,
+    width: '100px',
+    height: '100px',
+    x_out:'100px', // Ajout de l'unité 'px'
+    y_out:'200px', // Ajout de l'unité 'px'
+    x_in:'200px',
+    y_in:'200px',
+    page: 0, // Apparaît sur la première double-page (pages 1-2)
+    side: 'left', // Apparaît sur la page de gauche
+    type:'kitchenCarburant'
+  },
+  {
+    id:"cube-5",
+    img_src:'assets/block_rune_louche.png',
+    find: false,
+    stocked:false,
+    opacity: 0.2,
+    width: '100px',
+    height: '100px',
+    x_out:'100px', // Ajout de l'unité 'px'
+    y_out:'200px', // Ajout de l'unité 'px'
+    x_in:'200px',
+    y_in:'200px',
+    page: 0, // Apparaît sur la première double-page (pages 1-2)
+    side: 'left', // Apparaît sur la page de gauche
+    type:'kitchenRune'
+  },
+  {
+    id:"cube-6",
+    img_src:'assets/block_louche.png',
+    find: false,
+    stocked:false,
+    opacity: 0.2,
+    width: '100px',
+    height: '100px',
+    x_out:'100px', // Ajout de l'unité 'px'
+    y_out:'200px', // Ajout de l'unité 'px'
+    x_in:'200px',
+    y_in:'200px',
+    page: 0, // Apparaît sur la première double-page (pages 1-2)
+    side: 'left', // Apparaît sur la page de gauche
+    type:'kitchenOutil'
   }
 ];
