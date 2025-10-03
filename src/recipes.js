8a7ec68dae145a4c448f3490d600a68461ea3789
guillaume mondou <guillaume.mondou@epitech.eu>
2025-10-03 10:42:03 +0200

    feat: add olivier cake recette

--- src/recipes.js
+++ src/recipes.js
@@ -10,10 +10,37 @@ export const recipes = [
     ingredients: {
       // Les IDs des cubes originaux requis pour la recette
       kitchenReceptacle: 'petit_chaudron', // Le chaudron va dans la zone "réceptacle"
-      kitchenBag: ['pomme', 'glachon'], // Les ingrédients vont dans "l'atelier"
+      kitchenBag: [, 'glachon', 'pomme'], // Les ingrédients vont dans "l'atelier"
       kitchenOutil: 'louche',
       kitchenRune: 'rune_louche',
       kitchenCarburant: 'charbon',
+    },
+  },
+  {
+    name: 'olivier_cake',
+    result: {
+      // L'ID du cube original 'poto-bois' qui doit exister dans cubes.js
+      originalCubeId: 'poto-bois',
+      img_src: 'assets/page_cake_olivier_1.png',
+      type: 'result', // Un type spécifique pour les objets fabriqués
+    },
+    ingredients: {
+      // Les IDs des cubes originaux requis pour la recette
+      kitchenReceptacle: 'cake_moule', // Le chaudron va dans la zone "réceptacle"
+      kitchenBag: [
+        'farine',
+        'levure',
+        'oeuf',
+        'huile',
+        'vin_blanc',
+        'porto',
+        'olivier_pousse',
+        'jambon',
+        'gruyere',
+      ], // Les ingrédients vont dans "l'atelier"
+      kitchenOutil: 'batteur',
+      kitchenRune: 'rune_batteur',
+      kitchenCarburant: 'beure',
     },
   },
   // ... vous pourrez ajouter d'autres recettes ici
